70d193d55bf5cc2a736a6f750483889759beb64f
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-10 15:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	 shortHex = shortHex.replace("#", "");
+
+    // Check if the input is a short hex code with 3 characters
+    if (shortHex.length === 3) {
+        // Convert each short hex character to a full hex component (e.g., "a" to "aa")
+        const fullHex = shortHex.split("").map((char) => char + char).join("");
+
+        // Add "#" to the beginning and return the full hex code
+        return "#" + fullHex.toUpperCase(); // Convert to uppercase for consistency
+    }
+
+    // Return the input as-is if it's not a short hex code
+    return shortHex;
 };
 
 // Do not change the code below.
